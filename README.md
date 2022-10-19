24cffb2c3b12123fa49c96d48c5811efcf3089ba
mike6321 <mike6321@naver.com>
2022-10-19 20:05:04 +0900

    feat :  리더 선출 알고리즘 구현

--- README.md
+++ README.md
@@ -8,3 +8,20 @@ https://downloads.apache.org/zookeeper/stable/
 * cd /Users/nhn/IdeaProjects/repository/apache-zookeeper-3.8.0-bin/bin
 
 * ./zkServer.sh
+
+# 리더 선출 알고리즘 구현
+
+### 노드 생성
+
+* ./zkCli.sh
+* create /election ""
+
+### 메이븐 패키징 및 실행
+
+* mvn clean package
+* java -jar target/distributed-system-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+### 결과
+
+<img width="2559" alt="image" src="https://user-images.githubusercontent.com/33277588/196673399-aae6fde3-1d21-44d3-ab38-f8dba6adfd0f.png">
+
--- src/main/java/LeaderElection.java
+++ src/main/java/LeaderElection.java
@@ -1,21 +1,46 @@
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class LeaderElection implements Watcher {
 
     private static final String ZOOKEEPER_ADDRESS = "127.0.0.1:2181";
     private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
     private ZooKeeper zooKeeper;
+    private String currentZNodeName;
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
         LeaderElection leaderElection = new LeaderElection();
         leaderElection.connectionZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
         leaderElection.run();
         leaderElection.close();
         System.out.println("Disconnected from Zookeeper, exiting application");
+    }
+
+    public void volunteerForLeadership() throws InterruptedException, KeeperException {
+        String zNodePrefix = ELECTION_NAMESPACE + "/c_";
+        // EPHEMERAL_SEQUENTIAL : 주키퍼와 연결이 끊어지면 해당 Z노드 삭제
+        String zNodeFullPath = zooKeeper.create(zNodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("zNodeFullPath = " + zNodeFullPath);
+        this.currentZNodeName = zNodeFullPath.replace(ELECTION_NAMESPACE + "/election/", "");
+    }
+
+    public void electLeader() throws InterruptedException, KeeperException {
+        List<String> children = zooKeeper.getChildren(ELECTION_NAMESPACE, false);
+        Collections.sort(children);
+        String smallestChild = children.get(0);
+
+        if (smallestChild.equals(currentZNodeName)) {
+            System.out.println("I am the leader");
+            return;
+        }
+        System.out.println("I am not the leader, " + smallestChild + " is  the leader.");
     }
 
     public void connectionZookeeper() throws IOException {
