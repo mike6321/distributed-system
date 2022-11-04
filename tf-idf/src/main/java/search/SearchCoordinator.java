c74ae24e5bd8103bfa95524ee4fa8a915784441e
mike6321 <mike6321@naver.com>
2022-11-04 11:15:15 +0900

    feat : 검색을 위한 분산 코디네이터 - 분산 검색 (2)

--- tf-idf/src/main/java/search/SearchCoordinator.java
+++ tf-idf/src/main/java/search/SearchCoordinator.java
@@ -1,17 +1,18 @@
 package search;
 
 import cluster.management.ServiceRegistry;
+import com.google.protobuf.InvalidProtocolBufferException;
+import model.DocumentData;
 import model.Result;
 import model.SerializationUtils;
 import model.Task;
+import model.proto.SearchModel;
 import networking.OnRequestCallback;
 import networking.WebClient;
+import org.apache.zookeeper.KeeperException;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
@@ -39,7 +40,76 @@ public class SearchCoordinator implements OnRequestCallback {
 
     @Override
     public byte[] handleRequest(byte[] requestPayload) {
-        return new byte[0];
+        SearchModel.Request request = null;
+        try {
+            request = SearchModel.Request.parseFrom(requestPayload);
+            SearchModel.Response response = createResponse(request);
+
+            return response.toByteArray();
+        } catch (InvalidProtocolBufferException | InterruptedException | KeeperException e) {
+            e.printStackTrace();
+            return SearchModel.Response.getDefaultInstance().toByteArray();
+        }
+    }
+
+    private SearchModel.Response createResponse(SearchModel.Request request) throws InterruptedException, KeeperException {
+        SearchModel.Response.Builder searchResponse = SearchModel.Response.newBuilder();
+
+        System.out.println("Received search query : " + request.getSearchQuery());
+
+        List<String> searchTerms = TFIDF.getWordsFromLine(request.getSearchQuery());
+
+        List<String> workers = workersServiceRegistry.getAllServiceAddress();
+
+        if (workers.isEmpty()) {
+            System.out.println("No Search workers currently available");
+            return searchResponse.build();
+        }
+
+        List<Task> tasks = createTasks(workers.size(), searchTerms);
+        List<Result> results = sendTasksToWorkers(workers, tasks);
+
+        List<SearchModel.Response.DocumentStats> sortedDocuments = aggregateResults(results, searchTerms);
+        searchResponse.addAllRelevantDocuments(sortedDocuments);
+
+        return searchResponse.build();
+    }
+
+    private List<SearchModel.Response.DocumentStats> aggregateResults(List<Result> results, List<String> searchTerms) {
+        Map<String, DocumentData> allDocumentsResults = new HashMap<>();
+
+        for (Result result : results) {
+            allDocumentsResults.putAll(result.getDocumentToDocumentData());
+        }
+
+        System.out.println("Calculating score fore all the documents");
+
+        Map<Double, List<String>> documentsSortedByScore = TFIDF.getDocumentsSortedByScore(searchTerms, allDocumentsResults);
+
+        return sortDocumentsByScore(documentsSortedByScore);
+    }
+
+    private List<SearchModel.Response.DocumentStats> sortDocumentsByScore(Map<Double, List<String>> documentsSortedByScore) {
+        List<SearchModel.Response.DocumentStats> sortedDocumentsStatsList = new ArrayList<>();
+
+        for (Map.Entry<Double, List<String>> docScorePair : documentsSortedByScore.entrySet()) {
+            Double score = docScorePair.getKey();
+
+            for (String document : docScorePair.getValue()) {
+                File documentPath = new File(document);
+
+                SearchModel.Response.DocumentStats documentStats = SearchModel.Response.DocumentStats.newBuilder()
+                        .setScore(score)
+                        .setDocumentName(documentPath.getName())
+                        .setDocumentSize(documentPath.length())
+                        .build();
+
+                sortedDocumentsStatsList.add(documentStats);
+            }
+        }
+
+
+        return sortedDocumentsStatsList;
     }
 
     @Override
